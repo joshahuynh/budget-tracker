462a0264ffe784eca99608db155b00eaa1226d20
joshahuynh <joshahuynh@gmail.com>
2022-06-19 22:35:38 -0500

    update js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 const FILES_TO_CACHE = [
     "/",
-    "manifest.webmanifest",
+    "manifest.json",
     "index.html",
     "assets/css/styles.css",
     "assets/js/index.js",
@@ -16,7 +16,7 @@ const DATA_CACHE_NAME = "data-cache-v1";
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            console.log("Your files were pre-cached successfully!");
+            console.log("Your files were pre-cached");
             return cache.addAll(FILES_TO_CACHE);
         })
     );
@@ -30,7 +30,7 @@ self.addEventListener("activate", function (evt) {
             return Promise.all(
                 keyList.map(key => {
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
+                        console.log("Removing cache data", key);
                         return caches.delete(key);
                     }
                 })
--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 app.use(require("./routes/api.js"));
